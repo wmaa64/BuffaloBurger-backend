9dd97d13bb89907054fa4eb586aa4e0168573ea4
wael mohammed aly <wmaa64@yahoo.com>
2025-01-20 10:44:12 +0200

    2nd 20-1-25

--- server.js
+++ server.js
@@ -13,14 +13,30 @@ dotenv.config();
 
 const app = express()
 
-app.use(cors());
+//app.use(cors());
+
+// CORS configuration: Allow requests from both the frontend domain and localhost
+const allowedOrigins = [
+    'https://buffalo-burger-frontend.vercel.app',  // Production Frontend
+    'https://localhost:3000'                       // Local Development Frontend
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],            // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'],    // Allowed headers
+    credentials: true                                     // Allow credentials (cookies, headers)
+}));
+
 
 app.use(express.json());
 app.use(morgan('dev'));
-
-
-
-
 
 //Routes 
 app.use('/api/products', productRoutes);
